827422c454d28720b5652039eb21e89dd9ebb604
Gianluca Silletti <gianni3@bluewin.ch>
2022-05-18 11:29:45 +0200

    changes

---
+++ our_project.py
@@ -0,0 +1,28 @@
+# Install packages in the current Jupyter kernel
+!pip install yfinance
+!pip install plotly
+!pip install pandas
+!pip install numpy
+!pip install quandl
+!pip install pandas_datareader
+!pip install seaborn
+
+# Raw Package
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import quandl
+
+#import pandas_datareader
+import datetime as datetime
+from datetime import date
+import pandas_datareader
+
+import pandas_datareader.data as web
+
+# Data Source
+import yfinance as yf
+
+#Data viz
+import plotly.graph_objs as go
